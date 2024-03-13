f25e8a508c63875b627cfc113b42768ef90799b0
jpb <jakub.baranski.33@gmail.com>
2024-03-13 22:51:40 +0000

    add fluent ui

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
+
+import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <FluentProvider theme={teamsLightTheme}>
+        <App />
+    </FluentProvider>,
+    document.getElementById('root'),
 );
 
 reportWebVitals(sendToVercelAnalytics);
